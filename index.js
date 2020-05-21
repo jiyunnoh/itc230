98eb56b2fe3e74dca105118bcc6757402868e37a
Jiyun Noh <ianjy1127@gmail.com>
2020-05-21 10:39:17 +0900

    updated app.post

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ const employee = require("./data");
 const Employee = require("./models/employee")
 
 const app = express();
-app.engine('handlebars', exphbs({defaultLayout: false}));
+app.engine('handlebars', exphbs({ defaultLayout: false }));
 app.set("view engine", "handlebars");
 
 app.set('port', process.env.PORT || 3000);
-app.use(bodyParser.urlencoded({extended: true})); // parse form submissions. For every requests
+app.use(bodyParser.urlencoded({ extended: true })); // parse form submissions. For every requests
 //Your API should allow cross-origin resource sharing for appropriate routes, so it can be used by applications not on your web-site domain.
 app.use('/api', require('cors')()); // set Access-Control-Allow-Origin header for api route
 
@@ -22,8 +22,8 @@ app.use('/api', require('cors')()); // set Access-Control-Allow-Origin header fo
 app.get('/', (req, res) => {
   Employee.find({}).lean()
     .then((result) => {
-        res.render('home', { result });
-        // console.log(result);
+      res.render('home', { result });
+      // console.log(result);
     })
     .catch(err => next(err));
 });
@@ -31,49 +31,49 @@ app.get('/', (req, res) => {
 // send plain text response
 app.get('/about', (req, res) => {
   res.type('text/plain');
-  res.send('This is the about page.\n' + 'Hi. My name is Jiyun Noh. I am originally from South Korea.\n' + 
-            'I have been living in Seattle for a year now.\n' + 'I am studying Web development and database in SCC.');
- });
+  res.send('This is the about page.\n' + 'Hi. My name is Jiyun Noh. I am originally from South Korea.\n' +
+    'I have been living in Seattle for a year now.\n' + 'I am studying Web development and database in SCC.');
+});
 
- // return all records that match a condition
- app.get('/detail', function(req,res, next){
+// return all records that match a condition
+app.get('/detail', function (req, res, next) {
   // console.log(req.query)
   // var found = emp.get(req.query.name);
   // res.render("detail", {
   //     name: req.query.name, 
   //     result: found
   //     });
-return Employee.findOne({name: req.query.name}).lean()
-.then((selectedItem) => {
-  res.render('detail', { result: selectedItem });
-  console.log(selectedItem);
-})
-.catch(err => next(err));
+  return Employee.findOne({ name: req.query.name }).lean()
+    .then((selectedItem) => {
+      res.render('detail', { result: selectedItem });
+      console.log(selectedItem);
+    })
+    .catch(err => next(err));
 });
 
 //handle a POST request
-app.post('/detail', function(req, res, next) {
+app.post('/detail', function (req, res, next) {
   // console.log(req.body); // display parsed form submission
   // var found = data.get(req.body.name);
   // res.render('detail', {name: req.body.name, result: found, emp: data.getAll()});
 
   // return all records that match a condition
- return Employee.findOne({name: req.body.name}).lean()
+  return Employee.findOne({ name: req.body.name }).lean()
     .then((selectedItem) => {
-        res.render('detail', { result: selectedItem });
+      res.render('detail', { result: selectedItem });
     })
     .catch(err => next(err));
 
 });
 
 //Delete a requested item in DB
-app.get('/delete', function(req,res, next){
-return Employee.deleteOne({name: req.query.name}).lean()
-.then((deletedItem) => {
-  res.send('The number of deleted item is ' + deletedItem.deletedCount );
-  console.log(deletedItem);
-})
-.catch(err => next(err));
+app.get('/delete', function (req, res, next) {
+  return Employee.deleteOne({ name: req.query.name }).lean()
+    .then((deletedItem) => {
+      res.send('The number of deleted item is ' + deletedItem.deletedCount);
+      console.log(deletedItem);
+    })
+    .catch(err => next(err));
 });
 
 
@@ -81,8 +81,8 @@ return Employee.deleteOne({name: req.query.name}).lean()
 //api
 //get a single item
 app.get(('/api/v1/employee/:name'), (req, res, next) => {
-  Employee.findOne({name: req.params.name}, (err, result) => {
-    if(err || !result) return next(err);
+  Employee.findOne({ name: req.params.name }, (err, result) => {
+    if (err || !result) return next(err);
     res.json(result);
   });
 });
@@ -90,26 +90,40 @@ app.get(('/api/v1/employee/:name'), (req, res, next) => {
 //get all items
 app.get('/api/v1/employees', (req, res, next) => {
   Employee.find((err, result) => {
-    if(err || !result) return next(err);
+    if (err || !result) return next(err);
     res.json(result);
   });
 });
 
 //delete a single item
 app.get(('/api/v1/delete/:name'), (req, res, next) => {
-  Employee.deleteOne({name: req.params.name}, (err, result) => {
-    if(err || !result) return next(err);
-    res.json({"deleted": result.n});
+  Employee.deleteOne({ name: req.params.name }, (err, result) => {
+    if (err || !result) return next(err);
+    res.json({ "deleted": result.n });
   });
 });
 
+// //Add a new item or update an existing item
+// app.get(('/api/v1/add/:name/:age/:company/:position'), (req, res, next) => { 
+//   Employee.updateOne({name: req.params.name}, {name: req.params.name, 
+//                       age: req.params.age, company: req.params.company, 
+//                       position: req.params.position}, {upsert: true}, (err, result) => {
+//     if(err) return next(err);
+//     if(result.nModified == 0) {
+//       res.json("A new item has been added.");
+//     }
+//     else {
+//       res.json("The existing item has been modified.");
+//     }
+//   });
+// });
+
 //Add a new item or update an existing item
-app.get(('/api/v1/add/:name/:age/:company/:position'), (req, res, next) => { 
-  Employee.updateOne({name: req.params.name}, {name: req.params.name, 
-                      age: req.params.age, company: req.params.company, 
-                      position: req.params.position}, {upsert: true}, (err, result) => {
-    if(err) return next(err);
-    if(result.nModified == 0) {
+app.post(('/api/v1/add'), (req, res, next) => {
+  const newItem = req.body;
+  Employee.update({ name: newItem.name }, newItem, { upsert: true }, (err, result) => {
+    if (err) return next(err);
+    if (result.nModified == 0) {
       res.json("A new item has been added.");
     }
     else {
@@ -119,14 +133,14 @@ app.get(('/api/v1/add/:name/:age/:company/:position'), (req, res, next) => {
 });
 
 // define 404 handler. After any other routes
-app.use( (req,res) => {
-  res.type('text/plain'); 
+app.use((req, res) => {
+  res.type('text/plain');
   res.status(404);
   res.send('404 - Not found');
- });
+});
 
 app.listen(app.get('port'), () => {
-  console.log('Express started'); 
+  console.log('Express started');
 });
 
 
